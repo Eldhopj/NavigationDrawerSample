a61fcd58f30503911010c5631579095c14e83ec2
eldhopj <eldho@live.in>
2019-10-18 12:37:08 +0530

    Migrated into androidX

--- app/src/main/java/com/example/navigationdrawersample/MainActivity.java
+++ app/src/main/java/com/example/navigationdrawersample/MainActivity.java
@@ -1,19 +1,20 @@
 package com.example.navigationdrawersample;
 
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.design.widget.NavigationView;
-import android.support.v4.app.Fragment;
-import android.support.v4.view.GravityCompat;
-import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBarDrawerToggle;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
 
 import com.example.navigationdrawersample.fragment.AndroidFragment;
 import com.example.navigationdrawersample.fragment.ArchiveFragment;
 import com.example.navigationdrawersample.fragment.BackupFragment;
+import com.google.android.material.navigation.NavigationView;
 
 /**Add dependency
  * Create a menu, from this we populate the items into navigational drawer
